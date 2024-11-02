662dfd95c846d9668b5e4f56f63cd9f1314d6251
nuralivafoyev <nurikboy555@gmail.com>
2024-11-03 00:45:06 +0500

    added skeleton placeholder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import OrderCompleted from './order-completed';
 import OrderCanceled from './order-canceled';
 import Leads from './leads';
 import SignIn from './sign-in';
+import Skeleton from './skeleton';
 import Stats from './stats';
 
-export { OrderNew, OrderCompleted, OrderCanceled, Leads, SignIn, Stats };
+export { OrderNew, OrderCompleted, OrderCanceled, Leads, SignIn, Stats, Skeleton };
--- src/pages/stats/index.tsx
+++ src/pages/stats/index.tsx
@@ -1,19 +1,41 @@
 import { FC } from 'react';
+import { useEffect, useState } from 'react';
 import { StatCard } from '@/components';
 import { cardStats } from '@constants';
+import { Skeleton } from '..';
 import './_style.scss';
 
 const index: FC = () => {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Ma'lumotlarni yuklash simulyatsiyasi
+    const fetchData = () => {
+      setTimeout(() => {
+        setLoading(false);
+      }, 1500); // 2 soniya kutish
+    };
+
+    fetchData();
+  }, []);
   return (
-    <section className="stats">
-      <div className="container">
-        <div className="card-grid">
-          {cardStats.map((card) => (
-            <StatCard key={card.title} {...card} />
-          ))}
-        </div>
-      </div>
-    </section>
+    <>
+      {loading ? (
+        <Skeleton />
+      ) : (
+        <section className="stats">
+          <div className="container">
+            <div className="card-grid">
+              {cardStats.map((card) => (
+                <StatCard key={card.title} {...card} />
+              ))}
+            </div>
+          </div>
+        </section>
+      )
+      }
+    </>
   );
 };
 
