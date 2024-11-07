b48e8a4fabf6ad5fd6551f447f0dd7b0b454f85c
nuralivafoyev <nurikboy555@gmail.com>
2024-11-07 18:47:20 +0500

    fixed chart's bugs

--- src/components/ui/stat-charts/index.tsx
+++ src/components/ui/stat-charts/index.tsx
@@ -4,8 +4,14 @@ import './_style.scss';
 const index = () => {
   return (
     <div className="chart-container">
-      <div className="chart-wrapper">
+      <div className="chart-txt">
         <h1>Bar Chart Example</h1>
+        <h4>
+          Bar Chart is not works on mobile applications, please open with your
+          laptop!
+        </h4>
+      </div>
+      <div className="chart-wrapper">
         <BarChartComponent />
       </div>
     </div>
